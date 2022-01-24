71990be5c317c3025ce1ce7797edc0af92f3a5be
albilvl <>
2022-01-24 18:57:37 +0000

    update 1, building characterBar and eventListener

--- project/index.js
+++ project/index.js
@@ -1,2 +1,21 @@
 fetch("https://api.disneyapi.dev/characters")
-.then(charArr => console.log(charArr))
+  .then(res => res.json())
+  .then(characterObj => {
+    characterObj.data.forEach(character => {
+      renderCharacters(character)
+    })
+  })
+
+function renderCharacters(characters) {
+  const characterBar = document.querySelector(`#character-bar`)
+  const images = document.createElement(`img`)
+  images.src = character.imageUrl
+  images.style.cursor = `pointer`
+  characterBar.appendChild(images)
+  images.addEventListener(`click`,()=>{
+    document.querySelector(`#name`)= character.name
+    document.querySelector(`#image`).src = character.imageUrl
+    document.querySelector(`#image`).alt = character.name
+    
+  })
+}
