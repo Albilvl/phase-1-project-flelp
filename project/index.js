9b612fb7e2143dc8692ab7885a9b8d23310e269f
Steve Tatton <86009962+howsilence@users.noreply.github.com>
2022-01-25 19:02:20 +0000

    "html and css updates, JS now fetches entire db"

--- project/index.js
+++ project/index.js
@@ -1,31 +1,46 @@
+
+
 fetch("https://api.disneyapi.dev/characters")
-  .then(res => res.json())
-  .then(characterObj => {
-    characterObj.data.forEach(character => {
-      renderCharacters(character)
+.then(res => res.json())
+.then(characterObj => {
+  const page = characterObj.nextPage
+  for (i = 2; i < 150; i++){
+    fetch(`https://api.disneyapi.dev/characters?page=${i}`)
+    .then(res => res.json())
+    .then(page2data => {
+      page2data.data.forEach(character => {
+        renderCharacters(character)
+      })
     })
+
+  }
+  characterObj.data.forEach(character => {
+    renderCharacters(character)
   })
+})
 
 function renderCharacters(character) {
 
-  const characterBar = document.querySelector(`#character-bar`)
+const characterBar = document.querySelector(`#character-bar`)
 
-  const images = document.createElement(`img`)
+const images = document.createElement(`img`)
 
-  images.src = character.imageUrl
-  images.style.cursor = `pointer`
+images.src = character.imageUrl
+images.style.width = `10vw`
+images.style.height = `10vh`
+images.style.cursor = `pointer`
 
-  characterBar.appendChild(images)
+characterBar.appendChild(images)
 
-  images.addEventListener(`click`,()=>{
+images.addEventListener(`click`,()=>{
 
-    document.querySelector(`#name`).textContent= character.name
+  document.querySelector(`#name`).textContent= character.name
 
-    document.querySelector(`#image`).src = character.imageUrl
-    
-    document.querySelector(`#image`).alt = character.name
-    
-  })
+  document.querySelector(`#image`).src = character.imageUrl
+  
+  document.querySelector(`#image`).alt = character.name
+  
+})
 }
 
 // checking
